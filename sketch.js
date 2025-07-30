31ab818bd6bb153963eba176b80dfbbba3a849e9
Veronica Cristina <141179575+veronica-cristina@users.noreply.github.com>
2025-07-30 13:38:20 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ function setup() {
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
+  background(300, 210, 99); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
+  sphere(90, 300); //esfera 3D
   
   
 }
